d3758dc353890692f277dc5f11c0bc9420cf5218
Andrew King <andrew.king@acres4.com>
2015-07-21 10:43:30 -0700

    added version checking

--- bot.rb
+++ bot.rb
@@ -48,8 +48,23 @@ bot.on_message do |message, info|
       list_sites(type)
     end
   when "deployed"
+    messages = params.split(' ')
+    second_command = messages.first
+    messages.shift
+    case second_command
+    when "client"
+      #mesh-bot list sites
+      #mesh-bot list sites (production,dev)
+      site_name = messages.first
+      deployed_client_site(site_name)
+    when "version"
+      "Not implemented yet, blame Jonas"
+    end
     #mesh-bot deployed (sitename)
     #mesh-bot deployed version (version number)
+  when "help"
+    "mesh-bot help!\nAccepted commands:\n list sites [dev,production,prod] \n deployed client [sitename] \n deployed version [4.2.111]"
+
   else "what the fuck, you can't just yell my name in a channel and expect me to do shit for you! I am not an Usher!"
   end
 end
@@ -72,6 +87,34 @@ def list_sites(type=nil)
   end
 
   sites = site_list.sort.join("\n")
+end
+
+def deployed_client_site(site_name)
+  url = "https://#{@meshuser}:#{@meshpass}@mesh1.kailabor.com/mesh/sites/#{site_name}/deployments"
+  raw_response = RestClient.get url
+
+raw_response = JSON.parse(raw_response)
+  if !raw_response then 
+    response  = "No site named #{site_name} found!"
+  else  
+
+    version = raw_response["deploy"]["kai-apps"].split("/")[4]
+    jeb_name =  raw_response["deploy"]["kai-apps"].split("/").last.split("_").last
+
+    branch = raw_response["deploy"]["kai-apps"].split("/")[3]
+
+    full_version = version
+    build_list_url = "https://#{@meshuser}:#{@meshpass}@mesh1.kailabor.com/mesh/builds/branches/#{branch}/#{version}"
+    build_list = RestClient.get build_list_url
+    JSON.parse(build_list).each do | key,value |
+      if key.split(" ").join("-") == jeb_name then 
+        full_version = value["com.acres4.Kai"]["version"]
+      end
+    end
+
+     "#{site_name} has Kai client version: #{full_version}"
+  end
+
 end
 
 # actually start the bot
